210055cbb48e39768cafa229b166dfd70ce1a0ba
Findeton <felix@sequentech.io>
2022-06-08 17:08:38 +0100

    add prettier, styled components

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,34 @@
-import React from 'react'
-import './App.css'
-import Strand, {test} from 'strand'
+import React from "react"
+import "./App.css"
+import Strand, {test} from "strand"
+import styled from "styled-components"
 
-Strand().then(() => console.log('strand initialised'))
+Strand().then(() => console.log("strand initialised"))
 
 const runStrandTest = () => {
-  test()
+    test()
 }
 
+const StyledButton = styled.div`
+    background: gray;
+    cursor: pointer;
+    padding: 8px 16px;
+    border-radius: 10px;
+`
+
 const StrandButton: React.FC = () => {
-  return <div style={{background: 'gray', cursor: 'pointer'}} onClick={runStrandTest}>Click</div>
+    return <StyledButton onClick={runStrandTest}>Click</StyledButton>
 }
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Ballot verifier
-        </p>
-        <StrandButton />
-      </header>
-    </div>
-  )
+    return (
+        <div className="App">
+            <header className="App-header">
+                <p>Ballot verifier</p>
+                <StrandButton />
+            </header>
+        </div>
+    )
 }
 
 export default App
