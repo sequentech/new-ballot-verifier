db76e2ebd5e4479c61602abda8876996809242db
Findeton <felix@sequentech.io>
2022-06-09 15:19:34 +0100

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React from "react"
-import "./App.css"
 import Strand, {test} from "strand"
 import styled from "styled-components"
 
@@ -9,26 +8,101 @@ const runStrandTest = () => {
     test()
 }
 
-const StyledButton = styled.div`
+const Button = styled.div`
+    display: flex;
+    flex-grow: 0;
     background: gray;
     cursor: pointer;
     padding: 8px 16px;
     border-radius: 10px;
 `
 
+const StyledApp = styled.div`
+    display: flex;
+    flex-direction: column;
+    min-height: 100vh;
+    font-size: 14px;
+    line-height: 1.43;
+`
+const Header = styled.div`
+    display: flex;
+    flex-direction: row;
+    height: 40px;
+    width: 100%;
+    max-width: 1100px;
+    align-items: center;
+    margin-left: auto;
+    margin-right: auto;
+`
+const Footer = styled.div`
+    height: 40px;
+    background-color: #eee;
+    padding: 40px 0;
+    color: #333;
+    width: 100%;
+`
+
+const FooterWrapper = styled.div`
+    display: flex;
+    flex-direction: row;
+    width: 100%;
+    max-width: 1100px;
+    align-items: center;
+    margin-left: auto;
+    margin-right: auto;
+`
+
+const Body = styled.div`
+    display: flex;
+    flex-direction: column;
+    flex-grow: 2;
+    padding: 10px;
+    align-items: center;
+`
+
+const LogoImg = styled.img`
+    height: 20px;
+`
+
+const Link = styled.a`
+    font-weight: 700;
+    text-decoration: none;
+    color: #333;
+`
+
+const SocialWrapper = styled.div`
+    display: flex;
+    flex-grow: 2;
+    align-items: center;
+    justify-content: center;
+`
+
 const StrandButton: React.FC = () => {
-    return <StyledButton onClick={runStrandTest}>Click</StyledButton>
+    return <Button onClick={runStrandTest}>Click</Button>
 }
 
-function App() {
-    return (
-        <div className="App">
-            <header className="App-header">
-                <p>Ballot verifier</p>
-                <StrandButton />
-            </header>
-        </div>
-    )
-}
+const App = () => (
+    <StyledApp>
+        <Header>
+            <LogoImg src="Sequent_logo_small.png"></LogoImg>
+        </Header>
+        <Body>
+            <StrandButton />
+        </Body>
+        <Footer>
+            <FooterWrapper>
+                <p>
+                    Powered by{" "}
+                    <Link href="https://sequentech.io/" target="_blank">
+                        Sequent
+                    </Link>
+                </p>
+                <SocialWrapper>
+                    <p>Twitter</p>
+                </SocialWrapper>
+            </FooterWrapper>
+        </Footer>
+    </StyledApp>
+)
 
 export default App
