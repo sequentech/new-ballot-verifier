e79e7e5927964c736a90ecf6ee55195d255e1f0d
Findeton <felix@sequentech.io>
2022-07-15 16:43:33 +0100

    wip

--- browserstack/tests/specs/local_test.js
+++ browserstack/tests/specs/local_test.js
@@ -6,7 +6,7 @@ describe('wasm test local', () => {
   it('tests pass', async () => {
     //const ballotFixture = await fetch('../fixtures/ballot.json').then(res => res.json());
     const { default: ballotFixture } = await import("../../../fixtures/ballot.json", {assert: {type: "json"}});
-    await browser.url("http://localhost:3000/");
+    await browser.url("http://localhost:8080/");
     await browser.waitUntil(
       async () => (await browser.getTitle()).match("Ballot Verifier"),
       20000,
