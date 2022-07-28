abae1f3b5bb42a2b6c9296d122b77e2f0a4543c9
Findeton <felix@sequentech.io>
2022-07-28 16:35:09 +0100

    add missing changes

--- flake.nix
+++ flake.nix
@@ -9,9 +9,12 @@
   inputs.rust-overlay.url = "github:oxalica/rust-overlay";
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/3aa086b0f0ef1c7e7a8239326bdecb16bc1c3b7f"; # master branch 2022-07-18: 3aa086b0f0ef1c7e7a8239326bdecb16bc1c3b7f
   inputs.flake-utils.url = "github:numtide/flake-utils";
-
+  inputs.flake-compat = {
+    url = "github:edolstra/flake-compat";
+    flake = false;
+  };
   # output function of this flake
-  outputs = { self, nixpkgs, flake-utils, rust-overlay }:
+  outputs = { self, nixpkgs, flake-utils, rust-overlay, flake-compat }:
     flake-utils.lib.eachDefaultSystem (
       system:
         let
